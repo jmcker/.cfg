396e550545164b362ffe23eabbba599e3ef51c37
Jack McKernan <jmcker@outlook.com>
2018-03-20 23:17:55 -0500

    Create mount directory

--- .usb-backup.sh
+++ .usb-backup.sh
@@ -1,3 +1,4 @@
+sudo mkdir /media/usb-backup
 sudo mount -t vfat -o uid=pi,gid=pi /dev/sda1 /media/usb-backup/
 rsync -ravP --delete --ignore-existing --exclude .config/chromium --exclude .cache ~/ /media/usb-backup/$HOSTNAME-backup/
 sudo umount /media/usb-backup
