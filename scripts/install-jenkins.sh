6c9fedc774aad18d7e24a2bd8a9cdee0e217e72a
Jack McKernan <jmcker@outlook.com>
2018-04-11 02:18:06 -0400

    Add Jenkins to kvm group

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -4,3 +4,7 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
+
+sudo groupadd kvm
+sudo usermod -a -G kvm jenkins
+sudo chgrp kvm /dev/kvm
